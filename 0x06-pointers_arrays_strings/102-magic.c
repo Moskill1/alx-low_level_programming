f01792d1b341b96e641773c45695303c174478ff
Moskill1 <changolekibett@gmail.com>
2023-03-02 09:47:21 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
- * This is the main function.
- * It is the entry point of the program
- * Return: 0 (success)
- */
-
-int main(void);
-
+int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
-
-	/*
-	* Write your line of code here...
-	* Remember:
-	* - you are not allowed to use a
-	* - you are not allowed to modify p
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
-	*/
-
-	p = malloc(6 * sizeof(int)); /* Allocate memory for 6 integers */
-	*(p + 5) = 98;
-
-	/* ...so that this prints 98\n */
-
-	printf("%d\n", *(p + 5));
-	/* Print the value of the 6th integer in the block pointed to by p */
-
-	free(p); /* Free the memory allocated by malloc */
-
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
